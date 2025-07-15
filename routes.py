d3bcc03a5fbf1969f4453ad545e0b98430381cb6
vhxdevv <dacostadutravictorhugo@gmail.com>
2025-07-14 21:08:06 -0300

    primeiro commit

---
+++ routes.py
@@ -0,0 +1,11 @@
+from main import app
+from flask import render_template
+
+#rotas
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/blog")
+def blog():
+    return "BEM VINDO AO BLOG"
