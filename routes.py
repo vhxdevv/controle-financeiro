8f37162a490fd986add823b9b648993aeaa37511
vhxdevv <dacostadutravictorhugo@gmail.com>
2025-07-19 00:28:57 -0300

    Rota de CREATE da transacao adicionada e funcionando, melhorias a a fazer

--- routes.py
+++ routes.py
@@ -1,11 +1,24 @@
+from flask import render_template, redirect, request
+from extensions import db
 from main import app
-from flask import render_template
+from models import Transacao
 
 #rotas
+
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/blog")
-def blog():
-    return "BEM VINDO AO BLOG"
+@app.route("/cadastrar", methods=['POST'])
+def cadastrar():
+    descricao = request.form["descricao"]
+    valor = float(request.form["valor"])
+    tipo = request.form["tipo"]
+    categoria = request.form['categoria']
+    user_id = 1
+
+    nova_transacao = Transacao(descricao=descricao, valor=valor,categoria=categoria, tipo=tipo, user_id=1) #user id temporario
+    db.session.add(nova_transacao)
+    db.session.commit()
+
+    return redirect("/")
